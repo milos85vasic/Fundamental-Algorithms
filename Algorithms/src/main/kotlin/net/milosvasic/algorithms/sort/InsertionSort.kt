733695d8cb8aa555d5d179de7c4497b9a28dee94
Milos Vasic <milos85vasic@gmail.com>
2017-01-28 19:03:03 +0100

    Plain class for insertion sort.

---
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/InsertionSort.kt
@@ -0,0 +1,9 @@
+package net.milosvasic.algorithms.sort
+
+class InsertionSort<T> : Sort<T> {
+
+    override fun sort(elements: Collection<T>) {
+        throw UnsupportedOperationException("not implemented")
+    }
+
+}
---
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/Sort.kt
@@ -0,0 +1,8 @@
+package net.milosvasic.algorithms.sort
+
+
+interface Sort<T> {
+
+    fun sort(elements: Collection<T>)
+
+}
