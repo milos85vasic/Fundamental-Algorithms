2cf8c7144871f73759225c684dbc94df570f98c4
Milos Vasic <office@milosvasic.net>
2017-01-29 15:47:05 +0100

    Sorting descending.

--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/InsertionSort.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/InsertionSort.kt
@@ -3,18 +3,19 @@ package net.milosvasic.algorithms.sort
 class InsertionSort<T : Comparable<T>> : Sort<T> {
 
     override fun sort(elements: Array<T>, ascending: Boolean) {
-        if(ascending){
-            for (x in 1..elements.size - 1) {
-                val key = elements[x]
-                var y = x - 1
-                while (y >= 0 && elements[y] > key) {
-                    elements[y + 1] = elements[y]
-                    y--
-                }
-                elements[y + 1] = key
+        for (x in 1..elements.size - 1) {
+            val key = elements[x]
+            var y = x - 1
+            val condition = if (ascending) {
+                elements[y] > key
+            } else {
+                elements[y] < key
             }
-        } else {
-            
+            while (y >= 0 && condition) {
+                elements[y + 1] = elements[y]
+                y--
+            }
+            elements[y + 1] = key
         }
     }
 
