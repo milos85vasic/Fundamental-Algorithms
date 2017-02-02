fd4af0286afbc901608c483db79d45b7914f900c
Milos Vasic <office@milosvasic.net>
2017-02-02 19:50:01 +0100

    Compare algorithms.

---
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/CompareAlgoithms.kt
@@ -0,0 +1,26 @@
+package net.milosvasic.algorithms
+
+import net.milosvasic.algorithms.sort.InsertionSort
+import net.milosvasic.algorithms.sort.SelectionSort
+
+fun main(args: Array<String>) {
+    val tests = listOf(1, 10, 100, 1000, 10 * 1000, 100 * 1000)
+    val algorithms = listOf(InsertionSort<Int>(), SelectionSort<Int>())
+    for (algorithm in algorithms) {
+        for (count in tests) {
+            val data = getOrderedData(count, false) // Worst time consuming case for sorting!
+            val ascending = data.clone()
+
+            // Sort ascending
+            val start = System.currentTimeMillis()
+            SelectionSort<Int>().sort(ascending)
+            val elapsedAscending = getElapsedTime(start)
+
+            // Print data
+            // printData(data, ascending)
+            val name = algorithm.javaClass.simpleName
+            println("[ $name ] [ items count: $count ][ elapsed time: $elapsedAscending ]")
+        }
+        println("- - - - - - - - - - - - - - - ")
+    }
+}
