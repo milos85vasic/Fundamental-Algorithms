1d2fd457bcf9f370ab580f476b99ee89a36b1590
Milos Vasic <office@milosvasic.net>
2017-02-04 11:45:16 +0100

    Working on merge sort.

--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/MergeSort.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/MergeSort.kt
@@ -1,45 +1,28 @@
 package net.milosvasic.algorithms.sort
 
+import java.util.*
+
 
 class MergeSort<T : Comparable<T>> : Sort<T> {
 
     override fun sort(elements: Array<T>, ascending: Boolean) {
-        mergeSort(elements, 0, elements.lastIndex)
+        mergeSort(elements)
     }
 
-    private fun mergeSort(elements: Array<T>, start: Int, end: Int) {
-        if (start < end) {
-            val middle = (start + end) / 2
-            mergeSort(elements, start, middle)
-            mergeSort(elements, middle + 1, end)
-            merge(elements, start, end)
+    private fun mergeSort(elements: Array<T>) {
+        val size = elements.size
+        if (size > 1) {
+            val middle = size / 2
+            val first = Arrays.copyOfRange(elements, 0, middle - 1)
+            val second = Arrays.copyOfRange(elements, middle, size - 1)
+            mergeSort(first)
+            mergeSort(second)
+            merge(first, second)
         }
-//
-//
-//        if (p < r) {
-//            val q = (p + r) / 2
-//            println("$p $q $r")
-//            mergeSort(elements, p, q)
-//            mergeSort(elements, q, r)
-//            merge(elements, p, q, r)
-//        }
     }
 
-    private fun merge(elements: Array<T>, start: Int, end: Int) {
-        val middle = (start + end) / 2
-        var iFirst = 0
-        var iSecond = 0
-        var iMerged = 0
-        while (iFirst < middle && iSecond < end) {
-            if (elements[iFirst] < elements[iSecond]) {
-                elements[iMerged] = elements[iFirst]
-                iFirst++
-            } else {
-                elements[iMerged] = elements[iSecond];
-                iSecond++
-            }
-            iMerged++
-        }
+    private fun merge(first: Array<T>, second: Array<T>) {
+
     }
 
 }
