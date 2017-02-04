fd94ab68c06fbd51545b7193adf0f4deba165c01
Milos Vasic <office@milosvasic.net>
2017-02-04 14:58:52 +0100

    Added merge sort for algorithms comparison.

--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/CompareAlgoithms.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/CompareAlgoithms.kt
@@ -1,11 +1,12 @@
 package net.milosvasic.algorithms
 
 import net.milosvasic.algorithms.sort.InsertionSort
+import net.milosvasic.algorithms.sort.MergeSort
 import net.milosvasic.algorithms.sort.SelectionSort
 
 fun main(args: Array<String>) {
     val tests = listOf(1, 10, 100, 1000, 10 * 1000, 100 * 1000)
-    val algorithms = listOf(InsertionSort<Int>(), SelectionSort<Int>())
+    val algorithms = listOf(InsertionSort<Int>(), SelectionSort<Int>(), MergeSort<Int>())
     for (algorithm in algorithms) {
         for (count in tests) {
             val data = getOrderedData(count, false) // Worst time consuming case for sorting!
