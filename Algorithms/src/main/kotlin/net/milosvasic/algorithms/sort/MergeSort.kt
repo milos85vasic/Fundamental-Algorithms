873d7803fd946bc6e6492fec6aa1ca934c1a8dfb
Milos Vasic <office@milosvasic.net>
2017-02-05 20:17:02 +0100

    Merge sort: Fixing performance - using only one data structure to hold the data.

--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/MergeSort.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/MergeSort.kt
@@ -4,32 +4,55 @@ package net.milosvasic.algorithms.sort
 class MergeSort<T : Comparable<T>> : Sort<T> {
 
     override fun sort(elements: MutableList<T>, ascending: Boolean) {
-        mergeSort(elements, ascending)
+        mergeSort(elements, 0, elements.size - 1, ascending)
     }
 
-    private fun mergeSort(elements: MutableList<T>, ascending: Boolean) {
-        val size = elements.size
-        if (size < 2)
-            return
-        val mid = size / 2
-        val first = mutableListOf<T>()
-        val second = mutableListOf<T>()
-        for (i in 0..mid - 1) {
-            first.add(elements[i])
-
+    private fun mergeSort(elements: MutableList<T>, start: Int, end: Int, ascending: Boolean) {
+        if (start < end) {
+            val middle = (start + end) / 2
+            mergeSort(elements, start, middle, ascending)
+            mergeSort(elements, middle + 1, end, ascending)
+            merge(elements, start, middle, end, ascending)
         }
-        for (i in mid..size - 1) {
-            second.add(elements[i])
-        }
-        mergeSort(first, ascending)
-        mergeSort(second, ascending)
-        merge(first, second, elements, ascending)
     }
 
-    fun merge(first: MutableList<T>, second: MutableList<T>, elements: MutableList<T>, ascending: Boolean) {
-        val firstSize = first.size
-        val secondSize = second.size
+    fun merge(elements: MutableList<T>, start: Int, middle: Int, end: Int, ascending: Boolean) {
+        val firstSize = (middle + 1) - start
+        val secondSize = (end + 1) - (middle + 1)
         var i = 0
+        var j = 0
+        var k = 0
+        println("First size: $firstSize")
+        println("Second size: $secondSize")
+        while (i < firstSize && j < secondSize) {
+            if (elements[i + start] <= elements[j + middle + 1]) {
+                println(">>>>>> ${elements[i + start]}")
+                i++
+            } else {
+                println(">>>>>> ${elements[j + middle + 1]}")
+                j++
+            }
+            k++
+        }
+
+//        for(x in start..end){
+//            println("->>> ${elements[x]}")
+//        }
+
+        for (x in start..middle) {
+            println(elements[x])
+        }
+        println("--- --- ---")
+        for (x in middle + 1..end) {
+            println(elements[x])
+        }
+        println("---")
+
+
+
+        /*
+
+                var i = 0
         var j = 0
         var k = 0
         while (i < firstSize && j < secondSize) {
@@ -55,16 +78,9 @@ class MergeSort<T : Comparable<T>> : Sort<T> {
                 }
             }
         }
-        while (i < firstSize) {
-            elements[k] = first[i]
-            k++
-            i++
-        }
-        while (j < secondSize) {
-            elements[k] = second[j]
-            k++
-            j++
-        }
+
+
+         */
     }
 
 
