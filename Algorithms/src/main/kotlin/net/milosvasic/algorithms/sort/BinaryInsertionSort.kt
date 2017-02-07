2bb2f70b47ea3c64033ac9dcf07253971df88682
Milos Vasic <office@milosvasic.net>
2017-02-07 20:01:28 +0100

    Started work on binary insertion sort algorithm.

---
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/BinaryInsertionSort.kt
@@ -0,0 +1,9 @@
+package net.milosvasic.algorithms.sort
+
+class BinaryInsertionSort<T : Comparable<T>> : Sort<T> {
+
+    override fun sort(elements: MutableList<T>, ascending: Boolean) {
+
+    }
+
+}
