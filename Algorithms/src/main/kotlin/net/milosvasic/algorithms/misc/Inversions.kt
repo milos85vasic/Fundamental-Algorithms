c022cf1e7e9195c8453247a6e83caf5cf2468280
Milos Vasic <office@milosvasic.net>
2017-02-10 13:09:02 +0100

    Fundamental Algorithms: Working on inversions.

---
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/misc/Inversions.kt
@@ -0,0 +1,11 @@
+package net.milosvasic.algorithms.misc
+
+class Inversions<T : Comparable<T>> {
+
+    fun get(elements: List<T>): List<List<T>> {
+        val inversions = mutableListOf<List<T>>()
+        
+        return inversions
+    }
+
+}
