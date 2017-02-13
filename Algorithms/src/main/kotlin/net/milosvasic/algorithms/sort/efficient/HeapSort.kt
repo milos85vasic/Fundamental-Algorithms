dc769c8e33c88270f9ebf74159f5cb49d48f6358
Milos Vasic <office@milosvasic.net>
2017-02-13 20:47:52 +0100

    Fundamental Algorithms: Small refactoring.

--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/efficient/HeapSort.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/efficient/HeapSort.kt
@@ -4,16 +4,16 @@ import net.milosvasic.algorithms.sort.Sort
 
 class HeapSort<T : Comparable<T>> : Sort<T> {
 
-    private var total = 0
+    private var count = 0
 
     override fun sort(elements: MutableList<T>, ascending: Boolean) {
-        total = elements.size - 1
-        for (index in total / 2 downTo 0) {
+        count = elements.size - 1
+        for (index in count / 2 downTo 0) {
             heapify(elements, index, ascending)
         }
-        for (index in total downTo 1) {
+        for (index in count downTo 1) {
             swap(elements, 0, index)
-            total--
+            count--
             heapify(elements, 0, ascending)
         }
     }
@@ -23,17 +23,17 @@ class HeapSort<T : Comparable<T>> : Sort<T> {
         val left = index * 2
         val right = left + 1
         if (ascending) {
-            if (left <= total && elements[left] > elements[max]) {
+            if (left <= count && elements[left] > elements[max]) {
                 max = left
             }
-            if (right <= total && elements[right] > elements[max]) {
+            if (right <= count && elements[right] > elements[max]) {
                 max = right
             }
         } else {
-            if (left <= total && elements[left] < elements[max]) {
+            if (left <= count && elements[left] < elements[max]) {
                 max = left
             }
-            if (right <= total && elements[right] < elements[max]) {
+            if (right <= count && elements[right] < elements[max]) {
                 max = right
             }
         }
