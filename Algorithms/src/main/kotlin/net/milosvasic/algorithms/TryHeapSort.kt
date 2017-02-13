57ceccc58ec34d26f1b1a28b026e98520282775c
Milos Vasic <office@milosvasic.net>
2017-02-13 20:26:46 +0100

    Fundamental Algorithms: Working on descending heap sort.

--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/TryHeapSort.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/TryHeapSort.kt
@@ -5,30 +5,29 @@ import net.milosvasic.algorithms.sort.simple.InsertionSort
 
 fun main(args: Array<String>) {
     // Prepare data
-    val randoms = getRandomData(1000, 0, 1000)
+    val randoms = getRandomData(100 * 1000, 0, 1000)
     val ascending = mutableListOf<Int>()
-//    val descending = mutableListOf<Int>()
+    val descending = mutableListOf<Int>()
     ascending.addAll(randoms)
-//    descending.addAll(randoms)
+    descending.addAll(randoms)
 
     // Sort ascending
-    val start = System.currentTimeMillis()
+    var start = System.currentTimeMillis()
     HeapSort<Int>().sort(ascending)
     val elapsedAscending = getElapsedTime(start)
 
     // Sort descending
-//    start = System.currentTimeMillis()
-//    InsertionSort<Int>().sort(descending, false)
-//    val elapsedDescending = getElapsedTime(start)
+    start = System.currentTimeMillis()
+    InsertionSort<Int>().sort(descending, false)
+    val elapsedDescending = getElapsedTime(start)
 
     // Verify sorting
     verify(ascending)
-//    verify(descending, false)
+    verify(descending, false)
 
     // Print data
     println("Heap sort:")
-//    printData(randoms, ascending, descending)
-    printData(randoms, ascending)
+    //    printData(randoms, ascending, descending)
     println("Heap sort ascending, elapsed time: [ $elapsedAscending ]")
-//    println("Heap sort descending, elapsed time: [ $elapsedDescending ]")
+    println("Heap sort descending, elapsed time: [ $elapsedDescending ]")
 }
--- Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/efficient/HeapSort.kt
+++ Algorithms/src/main/kotlin/net/milosvasic/algorithms/sort/efficient/HeapSort.kt
@@ -9,28 +9,37 @@ class HeapSort<T : Comparable<T>> : Sort<T> {
     override fun sort(elements: MutableList<T>, ascending: Boolean) {
         total = elements.size - 1
         for (index in total / 2 downTo 0) {
-            heapify(elements, index)
+            heapify(elements, index, ascending)
         }
         for (index in total downTo 1) {
             swap(elements, 0, index)
             total--
-            heapify(elements, 0)
+            heapify(elements, 0, ascending)
         }
     }
 
-    private fun heapify(elements: MutableList<T>, index: Int) {
+    private fun heapify(elements: MutableList<T>, index: Int, ascending: Boolean) {
         var max = index
         val left = index * 2
         val right = left + 1
-        if (left <= total && elements[left] > elements[max]) {
-            max = left
-        }
-        if (right <= total && elements[right] > elements[max]) {
-            max = right
+        if (ascending) {
+            if (left <= total && elements[left] > elements[max]) {
+                max = left
+            }
+            if (right <= total && elements[right] > elements[max]) {
+                max = right
+            }
+        } else {
+            if (left <= total && elements[left] < elements[max]) {
+                max = left
+            }
+            if (right <= total && elements[right] < elements[max]) {
+                max = right
+            }
         }
         if (max != index) {
             swap(elements, index, max)
-            heapify(elements, max)
+            heapify(elements, max, ascending)
         }
     }
 
